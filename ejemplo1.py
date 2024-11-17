df8f32cbc363f76a8c40461dc34be60f6570d2e5
edgar <ragdepinedo@gmail.com>
2024-11-16 22:29:26 -0500

    commit inicial

---
+++ ejemplo1.py
@@ -0,0 +1,6 @@
+name = input("\033[92mcual es tu nombre:\n\033[0m")
+print(f"hola {name} vamos a realizar una suma.")
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+resultado = num1 + num2
+print(f"La suma de {num1} y {num2} es: {resultado}")
